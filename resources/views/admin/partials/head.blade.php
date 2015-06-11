f8e1e45dd3713cedb6a2e41d31b5589b647b59b1
jianwu <jianwu@sina.com>
2015-06-11 17:03:29 +0800

    update css

--- resources/views/admin/partials/head.blade.php
+++ resources/views/admin/partials/head.blade.php
@@ -23,8 +23,8 @@
 
     <link href="/plugins/iCheck/all.css" rel="stylesheet" type="text/css" />
 
-    <link rel="stylesheet" type="text/css" href="/plugins/simditor-2.1.5/styles/simditor.css" />
-	<link rel="stylesheet" type="text/css" href="/plugins/simditor-2.1.5/styles/simditor-html.css" />
+    <link rel="stylesheet" type="text/css" href="/plugins/simditor-2.1.13/styles/simditor.css" />
+	<link rel="stylesheet" type="text/css" href="/plugins/simditor-2.1.13/styles/simditor-html.css" />
 	
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
